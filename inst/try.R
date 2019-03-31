ae544c3a9d1549bda6f1a8c4cf9cd015c45afcc2
RobinHankin <hankin.robin@gmail.com>
2019-04-01 11:52:50 +1300

    whitespace and slicker idiom

--- inst/try.R
+++ inst/try.R
@@ -19,7 +19,6 @@ df <- function(x){
 x <- rnorm(9)
 print(df(x) %^% f(x))  # should be zero
 
-
 dw <- as.kform(1)
 dx <- as.kform(2)
 dy <- as.kform(3)
@@ -28,40 +27,61 @@ dz <- as.kform(4)
 f1 <- function(w,x,y,z){w*x*y*z + sin(x) + cos(w)}
 f2 <- function(w,x,y,z){w^2*x*y*z + sin(w) + w+z}
 f3 <- function(w,x,y,z){w*y + x + (w*z)}
-f4 <- function(w,x,y,z){sin(y+z) + x + (w*z)}
-f5 <- function(w,x,y,z){cos(w*z)}
-f6 <- function(w,x,y,z){x*y*z}
 
-phi <- (
+
+## I want a 2-form which is the sum of three elementary two-forms
+phi <-
+  (
     +f1(1,2,3,4) %^% dw %^% dx
     +f2(1,2,3,4) %^% dw %^% dy
     +f3(1,2,3,4) %^% dy %^% dz
+  )
 
-)
-# Df1 etc are numeric vectors corresponding to the first derivatives of f1() etc evaluated at point (1,2,3,4):
-Df1 <-    Deriv(f1)(1,2,3,4)
-Df2 <-    Deriv(f2)(1,2,3,4)
-Df3 <-    Deriv(f3)(1,2,3,4)
-Df4 <-    Deriv(f4)(1,2,3,4)  
+## We can use slightly slicker R idiom by defining elementary forms
+## e1,e2,e3 and then defining phi to be a linear sum, weighted with
+## 0-forms given by (scalar) functions f1(), f2() and f3():
+
+e1 <- dw %^% dx
+e2 <- dw %^% dy
+e3 <- dy %^% dz
+
+phi <-
+  (
+    +f1(1,2,3,4) %^% e1
+    +f2(1,2,3,4) %^% e2
+    +f3(1,2,3,4) %^% e3
+  )
+
+
+# Evaluate first derivatives of f1() etc at point (1,2,3,4), using Deriv():
+Df1 <- Deriv(f1)(1,2,3,4)
+Df2 <- Deriv(f2)(1,2,3,4)
+Df3 <- Deriv(f3)(1,2,3,4)
+# Df1 etc are numeric vectors of length 4
 
 
 ##Calculating dphi is easy:
-dphi <- (
-    +grad(Df1) %^% dw %^% dx
-    +grad(Df2) %^% dw %^% dy
-    +grad(Df3) %^% dy %^% dz
-)
+dphi <-
+  (
+    +grad(Df1) %^% e1
+    +grad(Df2) %^% e2
+    +grad(Df3) %^% e3
+  )
 
-## Now the Hessians at the same point:
+
+## Now work on the differential of the differential.  First evaluate
+## the Hessians (4x4 numeric matrices) at the same point:
+
 Hf1 <- matrix(Deriv(f1,nderiv=2)(1,2,3,4),4,4)
 Hf2 <- matrix(Deriv(f2,nderiv=2)(1,2,3,4),4,4)
 Hf3 <- matrix(Deriv(f3,nderiv=2)(1,2,3,4),4,4)
-Hf4 <- matrix(Deriv(f4,nderiv=2)(1,2,3,4),4,4)
+
 
 
 ## ddphi is clearly zero as the Hessians are symmetrical:
-ddphi <- (  # should be zero
-    +as.kform(which(Hf1 < Inf,arr.ind=TRUE),c(Hf1))
-    +as.kform(which(Hf2 < Inf,arr.ind=TRUE),c(Hf2))
-    +as.kform(which(Hf3 < Inf,arr.ind=TRUE),c(Hf3))
-)
+ddphi <- # should be zero
+  (  
+    +as.kform(which(!is.na(Hf1),arr.ind=TRUE),c(Hf1))
+    +as.kform(which(!is.na(Hf2),arr.ind=TRUE),c(Hf2))
+    +as.kform(which(!is.na(Hf3),arr.ind=TRUE),c(Hf3))
+  )
