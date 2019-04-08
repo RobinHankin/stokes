d16a7b9d6654076559b2a93735e5a77c88b29c63
RobinHankin <hankin.robin@gmail.com>
2019-04-09 11:15:40 +1200

    slight improvement, we need to test zero forms and nonzero forms separately

--- tests/testthat/test_alt.R
+++ tests/testthat/test_alt.R
@@ -8,11 +8,17 @@ test_that("Function Alt() behaves itself", {
     foo <- function(terms,k,n){
         S <- rtensor(terms,k,n,rnorm(terms))
         
-        error <- value(Alt(Alt(S)) - Alt(S)) # should be small
-        print(error)
-        expect_true(max(abs(error)) < 1e-8, info=S)
-     } # foo() closes
+        discrepancy <- Alt(Alt(S)) - Alt(S)
+        ## 'discrepancy should be zero in theory, but numerical
+        ## roundoff means discrepancy might not be the zero tensor.
 
+        if(!is.zero(discrepancy)){
+          error <- value(discrepancy)
+          expect_true(max(abs(error))<1e-8,info=dput(S))
+        } else {
+          expect_true(TRUE) 
+        }
+    } # foo() closes
 
     foo(4,5,6)
     foo(4,2,3)
@@ -20,4 +26,3 @@ test_that("Function Alt() behaves itself", {
     foo(8,5,6)
     foo(4,5,6)
 })
-
