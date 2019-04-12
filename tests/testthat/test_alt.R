3ad7e50eb09d3d99f7ff417df8e2ca172634e71d
RobinHankin <hankin.robin@gmail.com>
2019-04-12 17:08:26 +1200

    rationalize

--- tests/testthat/test_alt.R
+++ tests/testthat/test_alt.R
@@ -5,24 +5,25 @@ library("testthat")
 
 test_that("Function Alt() behaves itself", {
 
-    foo <- function(terms,k,n){
-        S <- rtensor(terms,k,n,rnorm(terms))
-        
+    foo <- function(S){
         discrepancy <- Alt(Alt(S)) - Alt(S)
         ## discrepancy should be zero in theory, but numerical
         ## roundoff means discrepancy might not be the zero tensor.
 
         if(!is.zero(discrepancy)){
-          error <- value(discrepancy)
-          expect_true(max(abs(error))<1e-8,info=dput(S))
+            error <- value(discrepancy)
+            expect_true(max(abs(error))<1e-8,info=dput(S))
         } else {
-          expect_true(TRUE) 
+            expect_true(TRUE) 
         }
     } # foo() closes
-
-    foo(4,5,6)
-    foo(4,2,3)
-    foo(4,1,6)
-    foo(8,5,6)
-    foo(4,5,6)
+    
+    
+    for(i in 1:10){
+        terms <- sample(2:4,1)
+        k <- sample(3:4,1)
+        n <- sample(2:4,1)
+        S <- rtensor(terms,k,n,rnorm(terms))
+        foo(S)
+    }
 })
