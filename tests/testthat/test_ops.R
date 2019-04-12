f3a4a64cb6891e96e25dc5cebf160185933e8469
RobinHankin <hankin.robin@gmail.com>
2019-04-12 13:24:32 +1200

    rationalize the tests for Ops

--- tests/testthat/test_ops.R
+++ tests/testthat/test_ops.R
@@ -1,58 +1,58 @@
 library("wedge")
 library("testthat")
 
+foo1 <- function(S){
+  expect_true(S == +S,info=S)
+  expect_true(S == -(-S),info=S)
+  expect_true(S - S == S*0,info=S)
+  expect_true(S + S == 2*S,info=S)
+  expect_true(S + S == S*2,info=S)
+  expect_true(S/2 + S/2 == S,info=S)
+} # foo1() closes
+
+foo2 <- function(S1,S2){
+  expect_true(S2 + S1 - S2 == S1,info=list(S1,S2))
+  expect_true(S1 + S2 == S2 + S1,info=list(S1,S2))
+}  # foo2() closes
+
 test_that("Cross product Ops behave", {
-
-    foo <- function(terms,k,n){
-        S  <- rtensor(terms,k,n,sample(terms))
-        S1 <- rtensor(terms,k,n,sample(terms))
-
-        expect_true(S == +S,info=S)
-        expect_true(S == -(-S),info=S)
-
-        expect_true(S - S == S*0,info=S)
-        expect_true(S + S == 2*S,info=S)
-        expect_true(S + S == S*2,info=S)
-        expect_true(S + S1 - S1 == S,info=list(S,S1))
-
-        expect_true(S + S1 == S1 + S,info=list(S,S1))
-        expect_true(S/2 + S/2 == S,info=S)
-        
-    }
-
-    foo(3,4,5)
-    foo(3,4,5)
-    foo(3,4,5)
-    foo(3,4,5)
-
+  for(i in 1:10){
+    terms <- rpois(1,20)
+    k <- sample(3:10,1)
+    n <- k+sample(3:10,1)
+    S <- rform(terms,k,n,sample(terms))
+    foo1(S)
+  }
+  
+  for(i in 1:10){
+    terms <- rpois(1,20)
+    k <- sample(3:10,1)
+    n <- k+sample(3:10,1)
+    S1 <- rform(terms,k,n,sample(terms))
+    S2 <- rform(terms,k,n,sample(terms))
+    foo2(S1,S2)
+  }
+  
 })
 
-      
-
 test_that("Wedge product Ops behave", {
-
-    foo <- function(terms,k,n){
-        S  <- rform(terms,k,n,sample(terms))
-        S1 <- rform(terms,k,n,sample(terms))
-
-        expect_true(S == +S,info=S)
-        expect_true(S == -(-S),info=S)
-
-        expect_true(S - S == S*0,info=S)
-        expect_true(S + S == 2*S,info=S)
-        expect_true(S + S == S*2,info=S)
-        expect_true(S + S1 - S1 == S,info=list(S,S1))
-
-        expect_true(S + S1 == S1 + S,info=list(S,S1))
-        expect_true(S/2 + S/2 == S,info=S)
-        
-    }
-
-    foo(3,4,5)
-    foo(3,4,5)
-    foo(3,4,5)
-    foo(3,4,5)
-
+  
+  for(i in 1:10){
+    terms <- rpois(1,20)
+    k <- sample(3:10,1)
+    n <- k+sample(3:10,1)
+    K <- rform(terms,k,n,sample(terms))
+    foo1(K)
+  }
+  
+  for(i in 1:10){
+    terms <- rpois(1,20)
+    k <- sample(3:10,1)
+    n <- k+sample(3:10,1)
+    K1 <- rform(terms,k,n,sample(terms))
+    K2 <- rform(terms,k,n,sample(terms))
+    foo2(K1,K2)
+  }
 })
 
       
