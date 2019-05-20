48e2f68b72b951d1f429348cb06ba3dfa9522dbc
RobinHankin <hankin.robin@gmail.com>
2019-05-20 21:48:21 +1200

    keep() and discard()

--- R/mult.R
+++ R/mult.R
@@ -290,3 +290,14 @@
   as.kform(index(omega),value(omega)*apply(M,1,prod))
 }
   
+`keep` <- function(omega,yes){
+    jj <- rep(0L,max(index(omega)))
+    jj[yes] <- 1
+    stretch(omega,jj)
+}
+
+`discard` <- function(omega,no){
+    jj <- rep(1L,max(index(omega)))
+    jj[no] <- 0L
+    stretch(omega,jj)
+}
