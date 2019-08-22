04c5ef0b3962d87b667a4fd190742ad5993f0038
RobinHankin <hankin.robin@gmail.com>
2019-08-22 15:04:42 +1200

    need an is.volume() function

--- R/mult.R
+++ R/mult.R
@@ -345,6 +345,8 @@
 
 `volume` <- function(n){as.kform(seq_len(n))}
 
+`is.volume` <- function(S){ (nterms(S) == 1) &&  all(seq_len(arity(S)) == index(volume(S))) }
+
 setGeneric("lose",function(x){standardGeneric("lose")})
 
 `lose` <- function(x){UseMethod("lose",x)}
