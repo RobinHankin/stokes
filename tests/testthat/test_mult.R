6d70ea99f8799a2f92f7ca93e1a7ee50285e9c9f
RobinHankin <hankin.robin@gmail.com>
2019-10-28 11:25:16 +1300

    more coverage

--- tests/testthat/test_mult.R
+++ tests/testthat/test_mult.R
@@ -15,4 +15,34 @@ test_that("coverage of functionality in mult.R", {
 
      expect_error(as.symbolic(function(x){x^5}))
 
+     expect_silent(as.symbolic(as.ktensor(+S)))
+     expect_silent(as.symbolic(as.ktensor(-S)))
+
+     expect_silent(hodge(as.kform(S)))
+     expect_error(hodge(0*volume(5)))
+     expect_silent(hodge(volume(5)))
+
+     expect_silent(hodge(0*volume(5),n=7))
+     expect_error(hodge(0*volume(5),n=3))
+
+     expect_error(hodge(scalar(6)))
+
+     expect_silent(hodge(scalar(6),n=3))
+     expect_silent(hodge(scalar(6),n=7))
+
+     expect_silent(hodge(spray(rbind(c(1,2,4,5)))))
+
+     expect_false(issmall(volume(5)))
+
+     K <- as.kform(spray(matrix(c(1,1,2,2,1,3,3,1,3,5),ncol=2,byrow=TRUE),1:5))
+     expect_error(stretch(K))
+     expect_silent(stretch(K,1:5))
+
+     expect_silent(keep(kform_general(7,3),1:4))
+     expect_silent(discard(kform_general(7,3),1))
+
+     expect_silent(zerotensor(5))
+     expect_error(zerotensor(-5))
+
+
 })
