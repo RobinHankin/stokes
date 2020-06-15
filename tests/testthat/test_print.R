8d94ad12e45f2184bf4f8bed70ea813be50a1000
RobinHankin <hankin.robin@gmail.com>
2020-06-16 11:46:09 +1200

    test the print methods

---
+++ tests/testthat/test_print.R
@@ -0,0 +1,16 @@
+## Some tests of the print methods
+
+options(warn=999)
+test_that("Functions print.ktensor() and print.kform() behave", {
+    expect_true(TRUE)
+
+    fooprint <- function(x){expect_output(print(x))}
+    for(i in 1:10){
+      x <- rform()
+      fooprint(x)
+      fooprint(x*0)
+      x <- rtensor()
+      fooprint(x)
+      fooprint(x*0)
+    }
+})
