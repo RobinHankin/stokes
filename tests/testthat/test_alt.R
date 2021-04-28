729bfe52576724b82525ad896addff8dbf27b3a1
RobinHankin <hankin.robin@gmail.com>
2021-04-29 10:48:02 +1200

    more testing for issue #48

--- tests/testthat/test_alt.R
+++ tests/testthat/test_alt.R
@@ -2,7 +2,7 @@
 
 test_that("Function Alt() behaves itself", {
 
-    foo <- function(S){
+    foo <- function(S){ # idempotence of Alt()
         discrepancy <- Alt(Alt(S)) - Alt(S)
         ## discrepancy should be zero in theory, but numerical
         ## roundoff means discrepancy might not be the zero tensor.
@@ -14,19 +14,26 @@ test_that("Function Alt() behaves itself", {
             expect_true(TRUE) 
         }
     } # foo() closes
-    
-    
+
+    bar <- function(S,V){   # Alt() is in fact alternating
+      S <- Alt(S)
+      if(ncol(V)>=2){
+        V1 <- V[,c(2,1,seq(from=3,to=ncol(V)))]
+        expect_true(abs(as.function(S)(V) + as.function(S)(V1)) < 1e-6)
+      }
+    }
+
     for(i in 1:10){
         terms <- sample(2:4,1)
         k <- sample(3:4,1)
         n <- sample(2:4,1)
         S <- rtensor(terms,k,n,rnorm(terms))
         foo(S)
+        V <- matrix(rnorm(k*n),ncol=k)
+        bar(S,V)
     }
 
     # test that issue  #48 has been resolved:
     foo(as.ktensor(matrix(c(4,2,4,3,4,2,2,3,3),3,3),1:3))
-
-
 
 })
