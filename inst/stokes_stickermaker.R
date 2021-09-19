1d1356455341338da076ac05649b283e35b42fff
Robin Hankin <rhankin@aut.ac.nz>
2021-09-19 22:01:27 +1200

    make package name bigger

--- inst/stokes_stickermaker.R
+++ inst/stokes_stickermaker.R
@@ -1,6 +1,6 @@
 library("hexSticker")
 
 imgurl <- "stokes_theorem.png"
-sticker(imgurl, package="stokes", p_size=8, s_x=1, s_y=.85,
+sticker(imgurl, package="stokes", p_size=24, s_x=1, s_y=.85,
 s_width=1, asp=0.85, white_around_sticker=TRUE, h_fill="#7733FF",
 h_color="#000000", filename="stokes.png")
