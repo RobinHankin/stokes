f08ad2c9b505b4f8aef4e64ccaf8d1d99eee6df4
Robin Hankin <hankin.robin@gmail.com>
2021-10-23 08:47:24 +1300

    use correct aspect ratio

--- inst/stokes_stickermaker.R
+++ inst/stokes_stickermaker.R
@@ -2,5 +2,5 @@ library("hexSticker")
 
 imgurl <- "stokes_theorem.png"
 sticker(imgurl, package="stokes", p_size=24, s_x=1, s_y=.85,
-s_width=1, asp=0.85, white_around_sticker=TRUE, h_fill="#7733FF",
+s_width=1, asp=sqrt(3)/2, white_around_sticker=TRUE, h_fill="#7733FF",
 h_color="#000000", filename="stokes.png")
