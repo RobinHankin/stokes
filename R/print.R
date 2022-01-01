e534af2722431f980e6fb739057d8afdb21d2171
Robin Hankin <hankin.robin@gmail.com>
2022-01-01 16:54:46 +1300

    fixes #57

--- R/print.R
+++ R/print.R
@@ -1,11 +1,12 @@
 `print.ktensor` <- function(x,...){
+    m <- max(0,index(x))
     if(is.zero(x)){
         cat(paste("The zero linear map from V^",arity(x)," to R with V=R^n:\n",sep=""))
     } else {
-        cat(paste("A linear map from V^",arity(x)," to R with V=R^",max(index(x)),":\n",sep=""))
+        cat(paste("A linear map from V^",arity(x)," to R with V=R^",m,":\n",sep=""))
     }
     if(isTRUE(getOption("stokes_symbolic_print"))){
-        cat(as.symbolic(x,d="d",symbols=as.character(seq_len(max(index(x))))),"\n")
+        cat(as.symbolic(x,d="d",symbols=as.character(seq_len(m))),"\n")
     } else {
         class(x) <- "spray"
         print(x,...)
@@ -14,13 +15,14 @@
 }
 
 `print.kform` <- function(x,...){
+    m <- max(0,index(x))
     if(is.zero(x)){
         cat(paste("The zero alternating linear map from V^",arity(x)," to R with V=R^n:\n",sep=""))
     } else {
-        cat(paste("An alternating linear map from V^",arity(x)," to R with V=R^",max(index(x)),":\n",sep=""))
+        cat(paste("An alternating linear map from V^",arity(x)," to R with V=R^",m,":\n",sep=""))
     }
     if(isTRUE(getOption("stokes_symbolic_print"))){
-        cat(as.symbolic(x,d="d",symbols=as.character(seq_len(max(index(x))))),"\n")
+        cat(as.symbolic(x,d="d",symbols=as.character(seq_len(m))),"\n")
     } else {
         class(x) <- "spray"
         print(x,...)
