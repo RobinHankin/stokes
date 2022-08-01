1eb7658bd117e7cd298384fdea4b3820b263b9df
Robin Hankin <hankin.robin@gmail.com>
2022-08-02 08:50:07 +1200

    increase coverage

--- tests/testthat/test_print.R
+++ tests/testthat/test_print.R
@@ -7,9 +7,35 @@ test_that("Functions print.ktensor() and print.kform() behave", {
     expect_output(print(a))
     expect_output(print(a*0))
   }
+
+  n <- 2
+
+
+  ##  check printing of ktensors:
+  options("ktensor_symbolic_print" = FALSE)
+  for(i in seq_len(n)){checker(rtensor())}
   
-  for(i in 1:10){
-    checker(rform())
-    checker(rtensor())
-  }
+  options("ktensor_symbolic_print" = TRUE)
+  for(i in seq_len(n)){checker(rtensor())}
+
+  options("ktensor_symbolic_print" = FALSE)  # revert to default
+  
+
+
+  ## check printing of ktensors:
+  options("kform_symbolic_print" = NULL)
+  for(i in seq_len(n)){checker(rform())}
+
+  options("kform_symbolic_print" = "dx")
+  for(i in seq_len(n)){checker(rform())}
+
+  options("kform_symbolic_print" = "txyz")
+  for(i in seq_len(n)){checker(rform())}
+
+  options("kform_symbolic_print" = "x") # generic non-null value
+  for(i in seq_len(n)){checker(rform())}
+
+  options("kform_symbolic_print" = FALSE)  # revert to default
+
+
 })
