2f4b80ab7b92281f9b77757de93f030af2e27f57
Robin Hankin <hankin.robin@gmail.com>
2023-08-18 10:50:23 +1200

    fixes #81

--- tests/testthat/test_transform.R
+++ tests/testthat/test_transform.R
@@ -2,7 +2,21 @@
 
 options(warn=999)
 test_that("Function pullback() behaves itself", {
-    expect_true(TRUE)
+  expect_true(TRUE)
+
+  `getgood` <- function(randex, test=function(...){TRUE}, maxit=1e5, default, ...){
+    it <- 0
+    while(it < maxit){
+      if(test(out <- eval(randex), ...)){return(out)}
+      it <- it+1
+    }
+    if(missing(default)){
+      stop("no good value found, try increasing maxit (or relaxing ftest()")
+    } else {
+      return(default)
+    }
+  }
+
 
     foo <- function(x,M){  # checks that pullback(pullback(x,M),solve(M)) == x
         xt <- pullback(pullback(x,M),solve(M))
@@ -12,7 +26,9 @@ test_that("Function pullback() behaves itself", {
 
     for(i in 1:3){
         o <-  rform(terms=3,k=2,n=5,coeffs=rnorm(3))
-        M <- matrix(rnorm(25),5,5)
+        randmat <- expression(matrix(rnorm(25),5,5))
+        isbigdet <- function(x,min_det = 0.01){abs(det(x)) > min_det}
+        M <- getgood(randmat, isbigdet, default=diag(5), min_det=0.1)
         foo(o,M)
         foo(o*0,M)
     }
