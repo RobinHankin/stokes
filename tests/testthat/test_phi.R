8be1158cdb9d6866642f3761d1b00214fbc8aebc
RobinHankin <hankin.robin@gmail.com>
2025-01-15 07:49:57 +0000

    typo

--- tests/testthat/test_phi.R
+++ tests/testthat/test_phi.R
@@ -6,10 +6,8 @@ test_that("phi() behaves as it should", {
     expect_true(as.function(phi(3))(as.matrix(e(4,5))) == 0)
   
     v <- sample(9)
-    expect_true(phi(v) == Reduce(`%X%`,sapply(v,phi))
+    expect_true(phi(v) == Reduce(`%X%`,sapply(v,phi)))
                 
 
 
 })
-
-      
